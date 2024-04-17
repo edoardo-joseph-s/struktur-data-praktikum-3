6c6fabe5d385e74564317b046d2f88e85bb16a47
Edaordo Sila <edoardosila07@gmail.com>
2024-04-18 02:42:53 +0700

    final header

--- nomor1.cpp
+++ nomor1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
@@ -7,10 +8,10 @@ int main() {
     int nim, kehadiran, tugas, quiz, uts, uas, nilai_akhir;
 
     // input data mahasiswa
-    for (int i = 1; i <= 6; i++)
+    for (int i = 1; i <= 2; i++)
     {
         cout << "\nData Siswa Ke-" << i;
-        cout << "Masukan Nama    : ";
+        cout << "\nMasukan Nama    : ";
         cin >> nama;
         cout << "Masukan NIM     : ";
         cin >> nim;
@@ -29,9 +30,11 @@ int main() {
     // menghitung nilai akhir
     nilai_akhir = kehadiran*0.05 + quiz*0.10 + tugas*0.15 + uts*0.35 + uas*0.35;
 
-    cout << left << setw(3) << "No" << setw(10) << "Nama" << setw(10) << "NIM" << setw(10) << "Kehadiran" << setw(10) << "Tugas" << setw(10) << "Quis" << setw << "UTS" << setw(10) << "UAS" << endl;
+    cout << "+--------------------------------------------------+-------------------+" << endl;
+    cout << left << setw(5) << "| No" << setw(10) << "| Nama" << setw(5) << "| NIM" << setw(10) << "| Kehadiran" << setw(10) << "| Tugas" << setw(10) << "| Quis" << setw(10) << "| UTS" << setw(10) << "| UAS" << "|" << endl;
+    cout << "+----------------------------------------------------------------------+" << endl;
 
-    for (int b = 1; b <= 6; b++)
+    for (int b = 1; b <= 2; b++)
     {
         /* code */
     }
