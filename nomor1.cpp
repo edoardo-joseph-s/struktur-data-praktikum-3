e5260eb53bfea1da15068a6c71193aa5b2819539
Edaordo Sila <edoardosila07@gmail.com>
2024-04-18 02:13:07 +0700

    looping pada input

---
+++ nomor1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // deklarasi variable
+    string nama;
+    int nim, kehadiran, tugas, quiz, uts, uas, nilai_akhir;
+
+    // input data mahasiswa
+    for (int i = 1; i <= 6; i++)
+    {
+        cout << "\nData Siswa Ke-" << i;
+        cout << "Masukan Nama    : ";
+        cin >> nama;
+        cout << "Masukan NIM     : ";
+        cin >> nim;
+        cout << "Nilai Kehadiran : ";
+        cin >> kehadiran;
+        cout << "Nilai Tugas     : ";
+        cin >> tugas;
+        cout << "Nilai Quiz      : ";
+        cin >> quiz;
+        cout << "Nilai UTS       : ";
+        cin >> uts;
+        cout << "Nilai UAS       : ";
+        cin >> uas;
+    }
+    
+
+
+
+
+
+}
