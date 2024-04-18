4711e4a8e30fdebd695a10e7f5b48dcb8d0bfa1d
Edaordo Sila <edoardosila07@gmail.com>
2024-04-19 01:46:32 +0700

    Membuat perbaikan pada tabel

--- nomor1.cpp
+++ nomor1.cpp
@@ -8,7 +8,7 @@ int main() {
     int nim, kehadiran, tugas, quiz, uts, uas, nilai_akhir;
 
     // input data mahasiswa
-    for (int i = 1; i <= 2; i++)
+    for (int i = 1; i <= 1; i++)
     {
         cout << "\nData Siswa Ke-" << i;
         cout << "\nMasukan Nama    : ";
@@ -31,16 +31,16 @@ int main() {
     nilai_akhir = kehadiran*0.05 + quiz*0.10 + tugas*0.15 + uts*0.35 + uas*0.35;
 
     // header rabel
-    cout << "+--------------------------------------------------+-------------------+" << endl;
-    cout << left << setw(5) << "| No" << setw(10) << "| Nama" << setw(5) << "| NIM" << setw(10) << "| Kehadiran" << setw(10) << "| Tugas" << setw(10) << "| Quis" << setw(10) << "| UTS" << setw(10) << "| UAS" << "|" << endl;
-    cout << "+----------------------------------------------------------------------+" << endl;
+    cout << "+------------------------------------------------------------------------------+" << endl;
+    cout << left << setw(5) << "| No" << setw(13) << "| Nama" << setw(10) << "| NIM" << setw(10) << "| Kehadiran" << setw(10) << "| Tugas" << setw(10) << "| Quis" << setw(10) << "| UTS" << setw(10) << "| UAS" << "|" << endl;
+    cout << "+------------------------------------------------------------------------------+" << endl;
 
     // menampikan data mahasiswa
-    for (int b = 1; b <= 2; b++)
+    for (int b = 1; b <= 1; b++)
     {
-        cout << left << setw(5) << "| " << b << setw(10) << "| " << nama << setw(10) << "| " << kehadiran << setw(10) << "| " << tugas << setw(10) << "| " << quiz << "| " << uts << "| " << uas << "|" << endl;
+    cout << left << setw(4) << "| " << b << setw(10) << "| " << nama << setw(8) << "| " << nim << setw(9) << "| " << kehadiran << setw(8) << "| " << tugas << setw(8) << "| " << quiz << setw(8) << "| " << uts << setw(8) << "| " << uas << "|" << endl;
     }
-    cout << "+----------------------------------------------------------------------+" << endl;
+    cout << "+------------------------------------------------------------------------------+" << endl;
     
     
     
