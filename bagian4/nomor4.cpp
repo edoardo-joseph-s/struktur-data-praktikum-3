cc1c498f36f957ebe97b4ef8d76a25a11f445635
Edaordo Sila <edoardosila07@gmail.com>
2024-04-23 06:57:48 +0700

    tambah nomor 4

---
+++ bagian4/nomor4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> larik;
+    int a = 0;
+
+    for (int i = 1; i <= 100; i += 2) {
+        larik.push_back(i);
+        a += i;
+    }
+
+    double rata2 = static_cast<double>(a) / larik.size();
+
+    cout << "Deret bilangan ganjil: ";
+    for (int i : larik)
+        cout << i << " ";
+
+    cout << "\nRata-rata: " << rata2 << endl;
+
+    return 0;
+}
