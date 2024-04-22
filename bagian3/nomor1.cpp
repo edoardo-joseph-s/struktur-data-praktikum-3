7721a5b8dda88f07262ceb9f18344e579dfe9541
Edaordo Sila <edoardosila07@gmail.com>
2024-04-23 05:00:51 +0700

    update

--- bagian3/nomor1.cpp
+++ bagian3/nomor1.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <iomanip>
-#include <string>
 using namespace std;
 
-struct DataMahasiswa {
+struct data_mhs {
     string nama; 
     int nim, kehadiran, tugas, quis, uts, uas;
     double akhir;
 };
 
 int main() {
-    const int jumlah_mahasiswa = 6;
-    DataMahasiswa mahasiswa[jumlah_mahasiswa];
+    data_mhs mahasiswa[6];
 
-    for (int i = 0; i < jumlah_mahasiswa; i++) {
+
+    for (int i = 0; i < 6 ;i++) {
         cout << "\nNomor. " << i + 1 << endl;
-        cout << "Masukkan Nama: ";
+        cout << "Masukan Nama: ";
         cin >> mahasiswa[i].nama;
-        cout << "Masukkan NIM: ";
+        cout << "Masukan NIM: ";
         cin >> mahasiswa[i].nim;
         cout << "Nilai Kehadiran: ";
         cin >> mahasiswa[i].kehadiran;
@@ -34,11 +33,11 @@ int main() {
     }
 
     cout << "------------------------------------------------------------------------------------------------" << endl;
-    cout << left << setw(5) << "| No" << setw(15) << "| Nama" << setw(10) << "| NIM" << setw(10) << "| Kehadiran" << setw(10) << "| Tugas" << setw(10) << "| Quiz" << setw(10) << "| UTS" << setw(10) << "| UAS" << setw(12) << "| Nilai Akhir" << "|" << endl;
+    cout << left << setw(5) << "| No" << setw(15) << "| Nama" << setw(10) << "| NIM" << setw(10) << "| Kehadiran" << setw(10) << "| Tugas" << setw(10) << "| Quiz" << setw(10) << "| UTS" << setw(10) << "| UAS" << setw(10) << "| Nilai Akhir" << "|" << endl;
     cout << "------------------------------------------------------------------------------------------------" << endl;
 
-    for (int i = 0; i < jumlah_mahasiswa; i++) {
-        cout << left << setw(5) << i + 1 << setw(15) << mahasiswa[i].nama << setw(10) << mahasiswa[i].nim << setw(10) << mahasiswa[i].kehadiran << setw(10) << mahasiswa[i].tugas << setw(10) << mahasiswa[i].quis << setw(10) << mahasiswa[i].uts << setw(10) << mahasiswa[i].uas << setw(12) << fixed << setprecision(2) << mahasiswa[i].akhir << endl;
+    for (int i = 0; i < 6; i++) {
+        cout << left <<setw(5) << i + 1 << setw(15) << mahasiswa[i].nama << setw(10) << mahasiswa[i].nim << setw(10) << mahasiswa[i].kehadiran << setw(10) << mahasiswa[i].tugas << setw(10) << mahasiswa[i].quis << setw(10) << mahasiswa[i].uts << setw(10) << mahasiswa[i].uas << setw(10) << mahasiswa[i].akhir << endl;
     }
     cout << "------------------------------------------------------------------------------------------------" << endl;
 
