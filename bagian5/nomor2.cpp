758c02483b135633814cdab0f0c8105df3af82df
Edaordo Sila <edoardosila07@gmail.com>
2024-04-23 05:33:23 +0700

    tambah no 2

---
+++ bagian5/nomor2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d, output;
+
+    cout << "Angka 1: ";
+    cin >> a;
+    cout << "Angka 2: ";
+    cin >> b;
+    cout << "Angka 3: ";
+    cin >> c;
+    cout << "Angka 4: ";
+    cin >> d;
+
+    output = a + b + c + d;
+
+    cout << "Hasil dari penjumlahan di atas adalah: " << output << endl;
+
+    return 0;
+}
