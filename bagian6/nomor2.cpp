b18c09df660ada70fe8c652c77b1a478c155f79b
Edaordo Sila <edoardosila07@gmail.com>
2024-04-23 05:42:17 +0700

    tambah nomor 2

---
+++ bagian6/nomor2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a1, a2, a3, a4, t;
+
+    cout << "A: ";
+    cin >> a1;
+    cout << "B: ";
+    cin >> a2;
+    cout << "C: ";
+    cin >> a3;
+    cout << "D: ";
+    cin >> a4;
+
+    tambah = a1 + a2 + a3 + a4;
+
+    cout << "Hasil dari A + B + C + D adalah: " << tambah << endl;
+
+    return 0;
+}
