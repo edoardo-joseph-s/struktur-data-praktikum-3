679d397a32589906a1d16e4d0acfcfb2f35409f8
Edaordo Sila <edoardosila07@gmail.com>
2024-04-23 07:13:43 +0700

    menambah nomor 7

---
+++ bagian3/nomor7.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct msk
+{
+    string judul, penyanyi;
+    int tahun, nt, data_lagu;
+    char code_album[3];
+};
+
+struct rbt_code
+{
+    msk rbt;
+};
+
+int main() {
+    vector<int> data;
+    rbt_code lagu[20];
+    int jumlahLagu;
+
+        int j_lagu;
+        cout << "Catatan: Masukan minimal 10 msk dan maximal 20 msk." << endl;
+        cout << "Masukan Jumlah Lagu: ";
+        cin >> jumlahLagu;
+
+        if (jumlahLagu < 10) {
+            cout << "Jumlah msk yang Anda masukan kurang dari 10." << endl;
+            return 1; 
+        } else if (jumlahLagu > 20) {
+            cout << "Jumlah msk yang Anda masukan lebih dari 20." << endl;
+            return 1; 
+        }
+
+        for (int i = 0; i < jumlahLagu; i++)
+        {
+            cout << "\nDate ke-" << i + 1 << ":\n";
+            cout << "Judul Lagu     : "; cin.ignore(); getline(cin, lagu[i].data_lagu.judul);
+            cout << "Penyanyi       : "; getline(cin, lagu[i].data_lagu.penyanyi);
+            cout << "Tahun Produksi : "; cin >> lagu[i].data_lagu.tahun;
+            cout << "Nomor Track    : "; cin >> lagu[i].data_lagu.nt;
+            cout << "Kode Album     : "; cin >> lagu[i].data_lagu.code_album;
+        }
+    
+    for (int i = 0; i < jumlahLagu; i++)
+    {
+        cout << "Data ke-" << i + 1 << "\n";
+        cout << "--------------------------------" << endl;
+        cout << "\tPenyanyi     : " << lagu[i].data_lagu.penyanyi << endl;
+        cout << "\tJudul Lagu   : " << lagu[i].data_lagu.judul << endl;
+        cout << "\tKode RBT     : " << lagu[i].rbt.nt << "-" << lagu[i].data_lagu.code_album << endl;
+        cout << "\tTahun        : " << lagu[i].data_lagu.tahun << endl;
+    }
+}
